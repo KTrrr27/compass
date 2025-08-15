793fba52852715d7c9e739e9066e392275d0f572
ryo27ttyo <ryo27.ttyo@gmail.com>
2025-08-16 00:16:44 +0900

    seeder 初期登録

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Users\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +16,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        User::create([
+            'over_name' => '紺羽栖',
+            'under_name' => '太郎',
+            'over_name_kana' => 'コンパス',
+            'under_name_kana' => 'タロウ',
+            'mail_address' => 'com_taro@com.com',
+            'sex' => '1',
+            'birth_day' => '1999/09/09',
+            'role' => '4',
+            'password' => 'com_taro'
+        ]);
     }
 }
