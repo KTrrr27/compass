8e595b66e48775cd59448a8eced80f2e40cffa34
ryo27ttyo <ryo27.ttyo@gmail.com>
2025-08-16 23:00:41 +0900

    多対多ファイル・クラス名変更

---
+++ app/Models/Users/Subject.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models\Users;
+
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\Users\User;
+
+//ファイル名クラス名変更
+class Subject extends Model
+{
+    const UPDATED_AT = null;
+
+
+    protected $fillable = [
+        'subject'
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(
+            User::class,
+            'subject_users',
+            'subject_id',
+            'user_id'
+        ); // リレーションの定義
+    }
+}
